16164c8cc5814975d2205e13a4f1bbefdb87010d
Rafael Schwan <rafaelschwan@hotmail.com>
2021-10-28 17:24:54 -0300

    Implementação da integração com cms prismic

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from "stream";
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
 
 // stripe listen --forward-to localhost:3000/api/webhooks
 
@@ -22,7 +23,9 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-  'checkout.session.completed'
+  'checkout.session.completed',
+  'customer.subscription.updated',
+  'customer.subscription.deleted',
 ]);
 
 // eslint-disable-next-line import/no-anonymous-default-export
@@ -42,11 +45,36 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const { type } = event;
 
     if (relevantEvents.has(type)) {
-      console.log('evento recebido', event);
-    } else {
-      res.json({ received: true });
+      try {
+        switch (type) {
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+            const subscription = event.data.object as Stripe.Subscription;
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false
+            );
+            break;
+          case 'checkout.session.completed':
+            const checkoutSession = event.data.object as Stripe.Checkout.Session;
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString(),
+              true
+            )
+            break;
+          default:
+            throw new Error('Unhandled event');
+        }
+      } catch (err) {
+        return res.json({
+          error: 'Webhook handler failed'
+        })
+      }
     }
 
+    res.json({ received: true });
   } else {
     res.setHeader('Allow', 'POST');
     res.status(405).end('Method not allowed');
